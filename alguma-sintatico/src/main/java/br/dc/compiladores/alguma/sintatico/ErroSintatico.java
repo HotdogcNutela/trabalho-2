de70575c3cf454b397d362e6fcdac3dd3ca54db4
HotdogcNutela <mvsoppp@gnail.com>
2023-06-12 22:50:50 -0300

    51/62 exemplos corretos

---
+++ alguma-sintatico/src/main/java/br/dc/compiladores/alguma/sintatico/ErroSintatico.java
@@ -0,0 +1,46 @@
+package br.dc.compiladores.alguma.sintatico;
+
+import java.io.PrintWriter;
+import java.util.BitSet;
+
+import javax.management.RuntimeErrorException;
+
+import org.antlr.v4.runtime.ANTLRErrorListener;
+import org.antlr.v4.runtime.Parser;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.atn.ATNConfigSet;
+import org.antlr.v4.runtime.dfa.DFA;
+
+public class ErroSintatico implements ANTLRErrorListener {
+    PrintWriter pw;
+    public ErroSintatico(PrintWriter pw) {
+        this.pw = pw;
+    }
+    
+    @Override
+    public void	reportAmbiguity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, boolean exact, BitSet ambigAlts, ATNConfigSet configs) {
+        // Sem alteracoes
+    }
+    
+    @Override
+    public void reportAttemptingFullContext(Parser recognizer, DFA dfa, int startIndex, int stopIndex, BitSet conflictingAlts, ATNConfigSet configs) {
+        // Sem alteracoes
+    }
+
+    @Override
+    public void reportContextSensitivity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, int prediction, ATNConfigSet configs) {
+        // Sem alteracoes
+    }
+
+    @Override
+    public void	syntaxError(Recognizer<?,?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+
+        Token t = (Token) offendingSymbol;
+
+        pw.println("Linha "+line+": erro sintatico proximo a "+t.getText());
+        pw.println("Fim da compilacao");
+        throw new RuntimeException();
+    }
+}
--- alguma-sintatico/src/main/java/br/dc/compiladores/alguma/sintatico/Principal.java
+++ alguma-sintatico/src/main/java/br/dc/compiladores/alguma/sintatico/Principal.java
@@ -1,6 +1,9 @@
 package br.dc.compiladores.alguma.sintatico;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -8,16 +11,29 @@ import org.antlr.v4.runtime.Token;
 
 public class Principal {
     public static void main(String args[]) throws IOException {
-        CharStream cs = CharStreams.fromFileName(args[0]);
-        AlgumaLexer lexer = new AlgumaLexer(cs);
+        try(PrintWriter pw = new PrintWriter(new File(args[1]))) {
 
-        // Descomentar para depurar o Léxico
-//        Token t = null;
-//        while( (t = lexer.nextToken()).getType() != Token.EOF) {
-//            System.out.println("<" + AlgumaLexer.VOCABULARY.getDisplayName(t.getType()) + "," + t.getText() + ">");
-//        }
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        AlgumaParser parser = new AlgumaParser(tokens);
-        parser.programa();
+            CharStream cs = CharStreams.fromFileName(args[0]);
+            AlgumaLexer lexer = new AlgumaLexer(cs);
+    
+            // Descomentar para depurar o Léxico
+    //        Token t = null;
+    //        while( (t = lexer.nextToken()).getType() != Token.EOF) {
+    //            System.out.println("<" + AlgumaLexer.VOCABULARY.getDisplayName(t.getType()) + "," + t.getText() + ">");
+    //        }
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            AlgumaParser parser = new AlgumaParser(tokens);
+            ErroSintatico errSintatico = new ErroSintatico(pw);
+            
+            // Remove a mensagem de erro padrão para então adicionar
+            // a mensagem de erro personalisada
+            parser.removeErrorListeners();
+            parser.addErrorListener(errSintatico);
+    
+            parser.programa();
+        } catch(RuntimeException ex) {
+            return;
+        }
+        
     }
 }
